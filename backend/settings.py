75cf45b79a8e038dbebb5a992c22ffdaf6c653d8
JerMa25 <tchamijerry@gmail.com>
2026-05-02 10:02:53 +0100

    update settings.py for railway

--- backend/settings.py
+++ backend/settings.py
@@ -157,10 +157,10 @@ CORS_ALLOW_CREDENTIALS = True
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'tchamijerry@gmail.com'
-EMAIL_HOST_PASSWORD = 'wkxe mujb tmec mizv'
+EMAIL_BACKEND = os.environ.get('EMAIL_BACKEND', 'django.core.mail.backends.smtp.EmailBackend')
+EMAIL_HOST = os.environ.get('EMAIL_HOST', 'smtp.gmail.com')
+EMAIL_PORT = int(os.environ.get('EMAIL_PORT', 587))
+EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS', 'True') == 'True'
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', 'tchamijerry@gmail.com')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', 'wkxe mujb tmec mizv')
 
