06f5556d82917dc4edaff921aa6d973b7fd41060
JerMa25 <tchamijerry@gmail.com>
2026-05-07 13:25:13 +0100

    allow endpoints from :     "http://192.168.221.224:3011"

--- backend/settings.py
+++ backend/settings.py
@@ -155,6 +155,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
     "http://localhost:8000",
     "http://127.0.0.1:8000",
+    "http://192.168.221.224:3011",  # Frontend URL
 ]
 
 CORS_ALLOW_CREDENTIALS = True
